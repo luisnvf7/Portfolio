3659de8984a052f4b7a4ee5cc30bb9bf0a023a2a
Luis Villalobos <villagusjs@gmail.com>
2020-08-28 20:23:27 -0400

    Updates

---
+++ precache-manifest.43fc77d6f0e5830e76ebd2034e6c5d1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e443ff9fa180bdbeb799f6bb10cb9e20",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "56b3f46ad28f06f37855",
+    "url": "/portfolio/static/css/2.559fd73f.chunk.css"
+  },
+  {
+    "revision": "695b5096d42b95102a88",
+    "url": "/portfolio/static/css/main.b581b047.chunk.css"
+  },
+  {
+    "revision": "56b3f46ad28f06f37855",
+    "url": "/portfolio/static/js/2.16e94d87.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/portfolio/static/js/2.16e94d87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "695b5096d42b95102a88",
+    "url": "/portfolio/static/js/main.6b1d8c83.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
